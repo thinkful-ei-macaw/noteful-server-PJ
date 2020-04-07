0ab420d566bbc8fc2bcaf27e323fbd8dc3bb43f2
Jordan <jordanknox@Jordans-MacBook-Pro-3.local>
2020-04-06 18:26:16 -0700

    finished folder router

--- src/notes/folder-router.js
+++ src/notes/folder-router.js
@@ -1,15 +1,10 @@
 const path = require('path');
 const express = require('express');
-const xss = require('xss');
 const folderService = require('./folder-service');
 
 const folderRouter = express.Router();
 const jsonParser = express.json();
 
-const serializeFolder = folder => ( {
-  id: folder.id,
-  title: folder.title
-});
 
 folderRouter
   .route('/:id')
@@ -18,10 +13,81 @@ folderRouter
       req.app.get('db'),
       req.params.id
     )
-    .then(folder => {
-      if(!folder)
-      return res.status(404).json({
-        error. {message: Folder does not exist}
+      .then(folder => {
+        if (!folder)
+          return res.status(404).json({
+            error: {
+              message: 'Folder does not exist'
+            }
+          });
+        else {
+          res.status(201).json(folder);
+        }
       })
-    })
+      .catch(next);
   })
+
+  .patch((req, res, next) => {
+    const {
+      title
+    } = req.body;
+    const folder = {
+      title
+    };
+    if (!title) {
+      return res.status(404).json({
+        error: 'Must include title'
+      });
+    }
+    folderService.updateFolder(
+      req.app.get('db'),
+      folder
+    );
+  })
+  .delete((req, res, next) => {
+    folderService.deleteFolder(
+      req.app.get('db'), 
+      req.params.id
+    )
+      .then(() => {
+        res.status(204).end();
+      })
+      .catch(next);
+  });
+
+folderRouter
+  .route('/')
+  .post((req, res, next) => {
+    const {
+      title
+    } = req.body;
+    const folder = {
+      title
+    };
+    if (!title) {
+      return res.status(404).json({
+        error: {
+          message: 'Must include title'
+        }
+      });
+    }
+
+    folderService.insertFolder(
+      req.app.get('db'),
+
+      folder)
+      .then(folder => {
+        res.json(folder);
+      });
+  })
+  .all((req, res, nexrt) => {
+
+  });
+
+
+
+
+
+
+
+module.exports = folderRouter;
